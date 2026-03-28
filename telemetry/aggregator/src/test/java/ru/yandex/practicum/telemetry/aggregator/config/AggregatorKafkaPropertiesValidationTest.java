97b1b9e945eeab155a2903b6b4202e2cdb9d56df
DokPlay <serrggeejjj@gmail.com>
2026-03-28 08:41:41 +0300

    Clean up aggregator validation test setup

--- telemetry/aggregator/src/test/java/ru/yandex/practicum/telemetry/aggregator/config/AggregatorKafkaPropertiesValidationTest.java
+++ telemetry/aggregator/src/test/java/ru/yandex/practicum/telemetry/aggregator/config/AggregatorKafkaPropertiesValidationTest.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.telemetry.aggregator.config;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -13,20 +12,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class AggregatorKafkaPropertiesValidationTest {
 
-    private Validator validator;
-
-    @BeforeEach
-    void setUp() {
-        validator = Validation.buildDefaultValidatorFactory().getValidator();
-    }
-
     @Test
     void shouldRejectSendTimeoutThatDoesNotExceedLinger() {
         AggregatorKafkaProperties properties = new AggregatorKafkaProperties();
         properties.setSendTimeout(Duration.ofMillis(5));
         properties.getProducer().setLingerMs(5);
 
-        Set<ConstraintViolation<AggregatorKafkaProperties>> violations = validator.validate(properties);
+        Set<ConstraintViolation<AggregatorKafkaProperties>> violations = validate(properties);
 
         assertThat(violations)
                 .extracting(ConstraintViolation::getMessage)
@@ -39,10 +31,17 @@ class AggregatorKafkaPropertiesValidationTest {
         properties.getProducer().setEnableIdempotence(true);
         properties.getProducer().setMaxInFlightRequestsPerConnection(6);
 
-        Set<ConstraintViolation<AggregatorKafkaProperties>> violations = validator.validate(properties);
+        Set<ConstraintViolation<AggregatorKafkaProperties>> violations = validate(properties);
 
         assertThat(violations)
                 .extracting(ConstraintViolation::getMessage)
                 .contains("aggregator.kafka.producer.maxInFlightRequestsPerConnection must be <= 5 when idempotence is enabled");
+    }
+
+    private Set<ConstraintViolation<AggregatorKafkaProperties>> validate(AggregatorKafkaProperties properties) {
+        try (var validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            Validator validator = validatorFactory.getValidator();
+            return validator.validate(properties);
+        }
     }
 }
