6af267a7966780809eb1c6676b79f9c9e696fe19
DokPlay <serrggeejjj@gmail.com>
2026-03-28 17:23:49 +0300

    Add safe defaults to Avro sensor payload schemas

--- telemetry/serialization/avro-schemas/src/test/java/ru/yandex/practicum/telemetry/serialization/AvroSchemaCompatibilityTest.java
+++ telemetry/serialization/avro-schemas/src/test/java/ru/yandex/practicum/telemetry/serialization/AvroSchemaCompatibilityTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AvroSchemaCompatibilityTest {
 
@@ -50,6 +51,16 @@ class AvroSchemaCompatibilityTest {
         assertEquals("ACTIVATE", enumDefault("src/main/avro/04-ActionTypeAvro.avsc"));
     }
 
+    @Test
+    void shouldKeepSensorPayloadFieldDefaultsDefinedForSchemaEvolution() throws IOException {
+        assertAllFieldsHaveDefaults("src/main/avro/12-ClimateSensorAvro.avsc");
+        assertAllFieldsHaveDefaults("src/main/avro/13-LightSensorAvro.avsc");
+        assertAllFieldsHaveDefaults("src/main/avro/14-MotionSensorAvro.avsc");
+        assertAllFieldsHaveDefaults("src/main/avro/15-SwitchSensorAvro.avsc");
+        assertAllFieldsHaveDefaults("src/main/avro/16-TemperatureSensorAvro.avsc");
+        assertAllFieldsHaveDefaults("src/main/avro/19-TemperatureSensorPayloadAvro.avsc");
+    }
+
     private List<String> payloadBranchNames(org.apache.avro.Schema schema, String fieldName) {
         return schema.getField(fieldName).schema().getTypes().stream()
                 .map(org.apache.avro.Schema::getName)
@@ -59,5 +70,13 @@ class AvroSchemaCompatibilityTest {
     private String enumDefault(String path) throws IOException {
         Schema schema = new Schema.Parser().parse(Files.readString(Path.of(path)));
         return schema.getEnumDefault();
+    }
+
+    private void assertAllFieldsHaveDefaults(String path) throws IOException {
+        Schema schema = new Schema.Parser().parse(Files.readString(Path.of(path)));
+        schema.getFields().forEach(field ->
+                assertTrue(field.hasDefaultValue(),
+                        () -> "Field '%s' in schema '%s' must define a default value".formatted(field.name(), path))
+        );
     }
 }
